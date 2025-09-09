77072e26e743a6ac9b5c5f845caf30ec618ce024
EduMaster Developer <edumaster@example.com>
2025-09-09 22:40:53 +0300

    Fix: force PostCSS instead of lightningcss

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  css: {
-    transformer: 'postcss', // ⬅️ تعطيل lightningcss
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
